0166411fdd4b171ad76303b6b42ade915c66211f
Jazzy-girl <rsw5713@g.rit.edu>
2025-05-16 14:51:06 -0400

    updated custom user to have email, user, pronouns, and re-migrated everything

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=False)
+    pronouns = models.CharField(max_length=50, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username',)
