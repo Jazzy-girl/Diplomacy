62695b3ff1c142f3769a7fb30cca4c35863946ac
Jazzy-girl <rsw5713@g.rit.edu>
2025-05-16 18:41:01 -0400

    updated settings.py and .env.example

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,11 +47,11 @@ LOGIN_REDIRECT_URL = '/'                      # Where to redirect after login
 
 # Email auth
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yourprovider.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your@email.com'
-EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_HOST_USER = os.getenv('EMAIL_ID') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
