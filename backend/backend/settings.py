b9604f1b02e8e9b68510508893fd5781e6c880ed
Jazzy-girl <rsw5713@g.rit.edu>
2025-05-15 20:48:50 -0400

    updated Setup and .env.example

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,9 +98,9 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
+        "NAME": os.getenv("DB_DATABASE"),
         "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
     }
