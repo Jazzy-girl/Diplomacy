e833bb93039a1ef5a55ded5e7c2ac40f595f0774
Jazzy-girl <rsw5713@g.rit.edu>
2025-05-16 23:04:04 -0400

    registering actually works now

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from allauth.account.views import ConfirmEmailView
+
 # Create your views here.
 
 class CreateUserView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class ReactConfirmEmailView(ConfirmEmailView):
+    def get(self, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.confirm(self.request)
+        return redirect("http://localhost:5173/login")
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,12 +45,16 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = 'username'  # or None if you removed it
 LOGIN_REDIRECT_URL = '/'                      # Where to redirect after login
 
 # Email auth
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_ID') 
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
+# ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = '/email-verified'
+# ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = '/email-verified'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
