2ee710711241e9066156b2e5642de1aa74d760ab
Jazzy-girl <rsw5713@g.rit.edu>
2025-05-18 17:11:55 -0400

    added link to settings in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import { ACCESS_TOKEN } from "../constants"
-
+import {Link} from "react-router-dom"
 
 function Home(){
     
-    return <div>Home</div>
+    return <>
+    <div>Home</div>
+    <p><Link to="/settings">Your Settings</Link></p>
+    </>
 }
 
 export default Home
