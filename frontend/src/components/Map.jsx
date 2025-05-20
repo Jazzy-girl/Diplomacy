35971ac6e0290c1c2441fcb19584518ac3ab5baa
Jazzy-girl <rsw5713@g.rit.edu>
2025-05-20 14:18:34 -0400

    deleted old territories JSON

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,6 +1,20 @@
 import react, { useState } from "react"
 import territories from "../assets/territories.json"
+/*
+Planned Method:
+Use JSON to store:
+- static map data
+- default unit locations and country owners
 
+When starting a game:
+- Read the JSON file, make an entry into the "Territories" table for each territory including the territory name and if it has a supply center, default owner
+- and make an entry into the "Units" table for each unit
+
+Does this work? Is there a better way to do this?
+
+Would it be better for the "Units" table to have a column for game_id, and either owner_player_id foreign key, or country_owner (TEXT) where the PlayersGames table has game_id, player_id, and assigned country (TEXT)
+
+*/
 function Map(){
     const [hover, setHover] = useState("");
     return <div className="flex justify-end p-4"><svg width={window.innerWidth} height={window.innerHeight} viewBox={`0 0 ${window.innerWidth/3} ${window.innerHeight/3}`} className="w-[800px] h-auto border shadow-md">
