198451d8c45c25e665dab3ead3fa249e7b546ee0
Jazzy-girl <rsw5713@g.rit.edu>
2025-05-20 16:30:19 -0400

    made unit test for units and territory creation

--- backend/api/receivers.py
+++ backend/api/receivers.py
@@ -3,13 +3,21 @@ from django.dispatch import receiver
 from .models import Game, Territory, Unit
 import json
 
+TERRITORIES_FILE = '/home/rywilson/DipProject/Diplomacy/frontend/src/assets/countrySetup.json'
+UNITS_FILE = '/home/rywilson/DipProject/Diplomacy/frontend/src/assets/countrySetup.json'
 @receiver(post_save, sender=Game)
 def create_territories_and_units_on_game_save(sender, instance, created, **kwargs):
     if created:
         # First make the Territories
-        with open('/home/rywilson/DipProject/Diplomacy/frontend/src/assets/countrySetup.json', 'r') as fTerrs:
+        with open(TERRITORIES_FILE, 'r') as fTerrs:
             data = json.load(fTerrs)
             for name, territory in data.items():
                 sc_exists = territory["sc"]
                 Territory.objects.create(game=instance, name=name, sc_exists=sc_exists)
         # Then make the Units
+        with open(UNITS_FILE, 'r') as fUnits:
+            data = json.load(fUnits)
+            for country, territories in data.items():
+                for territory, type in territories.items():
+                    location = Territory.objects.get(name=territory, game=instance)
+                    Unit.objects.create(game=instance, location=location, type=type,owner=country[0])
